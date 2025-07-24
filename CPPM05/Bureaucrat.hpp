43727e48e4a4491a52d8b8ce0d7d9e014c797593
Lourenco Antonio Dala <lantonio@c1r2s6.42luanda.com>
2025-07-24 13:04:36 +0100

    bureaucrat base class created

---
+++ CPPM05/Bureaucrat.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 12:58:18 by lantonio          #+#    #+#             */
+/*   Updated: 2025/07/24 13:04:02 by lantonio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat {
+	private:
+		const std::string	name;
+		int	grade;
+
+	std::string	getName(void) const;
+	int	getGrade(void);
+	void	incrementGrade(void);
+	void	decrementGrade(void);
+};
+
+#endif
